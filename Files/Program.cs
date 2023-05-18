d5b744ae180c889b121fd5db686426c019cefb1d
BudX_ol75@student.itstep.org <BudX_ol75@student.itstep.org>
2023-05-18 18:20:29 +0300

    files

---
+++ Files/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //In Main() method declare Dictionary PhoneBook for keeping pairs PersonName - 
+            //    PhoneNumber.
+            //    1) From file "phones.txt" read 9 pairs into PhoneBook.Write
+            //    only PhoneNumbers into file "Phones.txt".
+            //    2) Find and print phone number by 
+            //    the given name(name input from console)
+            //    3) Change all phone numbers,
+            //    which are in format 80######### into new format +380#########. 
+            //    The result write into file «New.txt»
+
+
+            //create dictionary with names and numbers
+            Console.WriteLine("Hello World!");
+            Dictionary<string, string> PhoneBook = new Dictionary<string, string>();
+            PhoneBook.Add("Petr", "+380672222222");
+            PhoneBook.Add("Vasiliy", "80672222223");
+            PhoneBook.Add("Oleg", "80672222224");
+            PhoneBook.Add("Nikolay", "+380672222225");
+            PhoneBook.Add("Volodimir", "80672222226");
+            PhoneBook.Add("Ruslan", "80672222227");
+            PhoneBook.Add("Nik", "80672222228");
+            PhoneBook.Add("Jhon", "80672222229");
+            PhoneBook.Add("Albert", "80672222233");
+            PhoneBook.Add("Karl", "80672222244");
+
+
+
+            //create folder and txt with Names and numbers
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DirectoryInfo dir = Directory.CreateDirectory($@"{desktopPath}/test_folder");
+
+            string fileName = "PNumbers.txt";
+            string filePath = Path.Combine(dir.FullName, fileName);
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            using (StreamWriter includPhonesnumber = File.CreateText(filePath))
+            {
+                foreach (var item in PhoneBook)
+                {
+                    includPhonesnumber.WriteLine(item);
+                }
+            }
+
+            //create new txt in folder with numbers only
+
+            // string desktopPathOnlyNumbers = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            // DirectoryInfo dirOnlyNumbers = Directory.CreateDirectory($@"{desktopPathOnlyNumbers}/Only_numbers");
+
+            string fileNameOnlyNumbers = "OnlyNumbers.txt";
+            string filePath2 = Path.Combine(dir.FullName, fileNameOnlyNumbers);
+            if (!File.Exists(filePath2))
+            {
+                File.Create(filePath2).Close();
+            }
+
+            string personName, phoneNumber;
+            List<string> arrPhones = new List<string>();
+            foreach (KeyValuePair<string, string> info in PhoneBook)
+            {
+                personName = info.Key;
+                phoneNumber = info.Value;
+                arrPhones.Add(phoneNumber);
+            }
+            using (StreamWriter includOnlyPhonesnumber = File.CreateText(filePath2))
+            {
+                foreach (var item in arrPhones)
+                {
+                    includOnlyPhonesnumber.WriteLine(item);
+                }
+            }
+
+            //find phonenumbers with name
+
+            using (StreamReader reader = File.OpenText(filePath))
+            {
+                string number = "not found";
+                string allText = reader.ReadToEnd();
+                Console.WriteLine(allText);
+                Console.WriteLine("Enter name to find number: ");
+                string find = Console.ReadLine();
+                if (PhoneBook.TryGetValue(find, out number))
+                {
+                    Console.WriteLine(find + "  " + number);
+                }
+                else
+                {
+                    Console.WriteLine("not found number");
+                }
+            }
+
+            //change "80" in "+380"
+
+            
+            string[] arrAllinfo = File.ReadAllLines(filePath2);
+            using (StreamWriter includOnlyPhonesnumber = new StreamWriter(filePath2,true))
+            {
+                
+                includOnlyPhonesnumber.WriteLine("\nCorrect format: ");
+                for (int i = 0; i < arrAllinfo.Length; i++)
+                {
+                    if (arrAllinfo[i].StartsWith("80"))
+                    {
+                        arrAllinfo[i] = "+3" + arrAllinfo[i].Substring(0);
+                    }
+                    includOnlyPhonesnumber.WriteLine(arrAllinfo[i]);
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
--- soft-serv-interface/Program.cs
+++ soft-serv-interface/Program.cs
@@ -4,10 +4,46 @@ using System.Collections.Generic;
 
 namespace soft_serv_interface
 {
+//    Develop interface IFlyable with method Fly() (Output information about bird or plane).
+//Create two classes Bird(with fields: name and canFly) and Plane(with fields: mark and highFly) ,
+//which implement interface IFlyable.
+//Create List of IFlyable objects.Add some Birds and Planes to it.Call Fly() method for every item from
+//the list of it.
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class Bird:IFlyable
+    {
+        public string name { get; set; }
+        public string canFly { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{name} +  {canFly}");
+        }
+
+    }
+
+    class Plane:IFlyable
+    {
+        public string mark { get; set; }
+        public string highFly { get; set; }
+      
+        public void Fly()
+        {
+            Console.WriteLine($"{ mark} + {highFly}");
+        }
+    }
+
     //interface ICompareTo
     //{
 
     //}
+
+
 
     interface IDeveloper
     {
@@ -67,32 +103,42 @@ namespace soft_serv_interface
                 return $"{ Tool }";
             }
 
-            public void Sort()
+            //public void Sort()
+            //{
+            //    Array.Sort(this.Tool.CompareTo);
+            //}
+
+            public int CompareTo(object obj)
             {
-                Array.Sort(this.Tool.CompareTo);
+                return string.Compare(Tool.ToString(), obj != null ? ((IDeveloper)obj).Tool.ToString() : string.Empty);
             }
-            //public int CompareTo(object obj)
-            //{
-            //    return  ;
-            //}
-            //public int CompareTo(object obj)
-            //{
-            //    return string.Compare(Tool.ToString(), obj != null ? ((IDeveloper)obj).Tool.ToString() : string.Empty);
-            //}
         }
         static void Main(string[] args)
         {
 
+            Bird bird1 = new Bird() { name = "parrot", canFly = "flies" };
+            Bird bird2 = new Bird() { name = "hang", canFly = "can not flies" };
+            Plane plane1 = new Plane() { mark = "Tu 144", highFly = "20 000m" };
+            Plane plane2 = new Plane() { mark = "Su 35", highFly = "20 000m" };
+            IFlyable[] flyables = new IFlyable[]
+            {
+               bird1,bird2,plane1,plane2
+            };
+            foreach (var item in flyables)
+            {
+                item.Fly();
+            }
 
-            Console.WriteLine("Hello World!");
-            Programmer programmer1 = new Programmer() { lenguange = "C++ (programmer)" };
-            Programmer programmer2 = new Programmer() { lenguange = "Java (programmer)" };
-            Programmer programmer3 = new Programmer() { lenguange = "C# (programmer)" };
-            Programmer programmer4 = new Programmer() { lenguange = "PHP (programmer)" };
-            Builder builder1 = new Builder() { Tool = "Hammer (builder)" };
-            Builder builder2 = new Builder() { Tool = "Axe (builder)" };
-            Builder builder3 = new Builder() { Tool = "Drill (builder)" };
-            Builder builder4 = new Builder() { Tool = "Paint (builder)" };
+
+            //Console.WriteLine("Hello World!");
+            //Programmer programmer1 = new Programmer() { lenguange = "C++ (programmer)" };
+            //Programmer programmer2 = new Programmer() { lenguange = "Java (programmer)" };
+            //Programmer programmer3 = new Programmer() { lenguange = "C# (programmer)" };
+            //Programmer programmer4 = new Programmer() { lenguange = "PHP (programmer)" };
+            //Builder builder1 = new Builder() { Tool = "Hammer (builder)" };
+            //Builder builder2 = new Builder() { Tool = "Axe (builder)" };
+            //Builder builder3 = new Builder() { Tool = "Drill (builder)" };
+            //Builder builder4 = new Builder() { Tool = "Paint (builder)" };
             //IDeveloper[] arrDeveloper = new IDeveloper[]
             //    {
             //            programmer1,
@@ -112,7 +158,7 @@ namespace soft_serv_interface
             //    Console.WriteLine(item);
             //}
 
-            Console.WriteLine(builder4.CompareTo(builder3)); 
+          //  Console.WriteLine(builder4.CompareTo(builder3)); 
 
 
             // List<IDeveloper> developers = new List<IDeveloper>();
