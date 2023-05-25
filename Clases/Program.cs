5a428573f4edf2724538124056e5bb5d277380c7
BudX_ol75@student.itstep.org <BudX_ol75@student.itstep.org>
2023-05-25 22:25:12 +0300

    1Merge remote-tracking branch 'origin/master'

--- Clases/Program.cs
+++ Clases/Program.cs
@@ -150,7 +150,6 @@ namespace Clases
                 Console.WriteLine(Area.Key.ToString(),Area.Value);
             }
 
-
         }
     }
 }
---
+++ Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        public abstract class Product
+        {
+            public string name { get;set; }
+        }
+         
+       public class 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
